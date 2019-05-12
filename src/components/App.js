8f81b9969ef096c7dc470b990703416948e3c3ce
david <davidbeglari@gamil.com>
2019-05-12 16:30:05 +0400

    Calculator for event binding

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,14 @@ import './App.css';
 import Header from "../shared/components/layout/Header";
 import Footer from "../shared/components/layout/Footer";
 import Content from "../shared/components/layout/Content";
-import ToDo from "./ToDo/ToDo";
-import Timer from "./Pomodoro/Timer";
-import Coins from "./Coins/Coins";
+import Calculator from "./Calculator/Calculator";
 
 function App() {
     return (
         <div className="App">
             <Header title="Welcome to To do task list"/>
             <Content>
-                <ToDo />
-                <Timer />
-                <Coins />
+                <Calculator />
             </Content>
             <Footer/>
         </div>
